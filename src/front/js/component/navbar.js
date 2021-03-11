4829b68d0f2bba26b0f12ea0ec5efa2763a36abe
JOSEPH-8546-cyber <manueljosephtrabajo@gmail.com>
2021-03-11 17:50:20 +0000

    fix navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,8 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Login } from "./popUpsLandingPage/login";
-import { SignUp } from "./popUpsLandingPage/signUp";
-import { NewCostumer } from "./newCostumer";
 
 export const Navbar = () => {
 	return (
@@ -10,16 +7,6 @@ export const Navbar = () => {
 			<Link to="/">
 				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
 			</Link>
-			<div>
-				<Login />
-			</div>
-			<hr />
-			<div>
-				<SignUp />
-			</div>
-			<div>
-				<NewCostumer />
-			</div>
 
 			<div className="ml-auto">
 				<Link to="/demo">
