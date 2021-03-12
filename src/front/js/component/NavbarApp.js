dbc2daee0a79537595aba1a6880105083a8abfbd
JOSEPH-8546-cyber <manueljosephtrabajo@gmail.com>
2021-03-12 17:30:56 +0000

    se hizo cambios en el side bar y se añadio link de username

--- src/front/js/component/NavbarApp.js
+++ src/front/js/component/NavbarApp.js
@@ -8,11 +8,10 @@ export const NavbarApp = () => {
 				<span className="navbar-brand mb-0 h1">PlanificApp</span>
 			</Link>
 			<div className="ml-auto">
-                <Link to="/userName">
-				<button type="button" className="btn btn-outline-dark rounded-circle" data-toggle="modal" data-target="#exampleModal7">
+              
+				<button type="button" className="btn btn-outline-dark rounded-circle">
 					<i className="fas fa-user-circle" />
 				</button>
-                </Link>
 				<span> </span>
 				<span className="navbar-brand mb-0 h1">user name</span>
 			</div>
