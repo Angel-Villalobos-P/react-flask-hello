c98620b1f0243ba8f07824fa217061bdad39a315
Ángel Villalobos <angel.villalobos2596@gmail.com>
2021-03-25 00:16:54 +0000

    Update

--- src/front/js/component/landingpage/CaracteristicaProyectos.js
+++ src/front/js/component/landingpage/CaracteristicaProyectos.js
@@ -8,8 +8,8 @@ export const CaracteristicaProyecto = () => (
 		<div className="col-6">
 			<div className="card">
 				<div className="card-body">
-					<h5 className="card-title">CliProyectosentes</h5>
-					<h6 className="card-subtitle mb-2 text-muted">Registro de clientes</h6>
+					<h5 className="card-title">Proyectos</h5>
+					<h6 className="card-subtitle mb-2 text-muted">Registro de proyectos</h6>
 					<p className="card-text">
 						Puedes registrar todos los proyectos en los que estás trabajando y darle seguimiento a la
 						división de tareas para una mayor organización
